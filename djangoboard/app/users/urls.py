f9eb64f7ecd98fbd457b8ecf2f6d8773901c259d
hbyyy <qjaduddl94@gmail.com>
2020-07-02 18:04:28 +0900

    cbv 이용해 구현

--- djangoboard/app/users/urls.py
+++ djangoboard/app/users/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from users.views import register
+from users.views import Register
 
 app_name = 'users'
 urlpatterns = [
-    path('register/', register, name='register'),
+    path('register/', Register.as_view(), name='register'),
 ]
--- djangoboard/app/users/views.py
+++ djangoboard/app/users/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from django.views.generic.base import View
+
 from users.models import TestUser
 
 
-def register(request):
-    if request.method == 'GET':
+# def register(request):
+#     if request.method == 'GET':
+#         return render(request, 'users/register.html')
+#     else:
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#         user = TestUser.objects.create(username=username, password=password)
+#         return redirect('users:register')
+
+class Register(View):
+    def get(self, request):
         return render(request, 'users/register.html')
-    else:
+
+    def post(self, request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = TestUser.objects.create(username=username, password=password)
