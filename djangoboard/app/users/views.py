6ac766b66fe1a07e6048690d16f499156e703e78
hbyyy <qjaduddl94@gmail.com>
2020-07-02 18:02:08 +0900

    register view 구현

--- djangoboard/app/users/views.py
+++ djangoboard/app/users/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from users.models import TestUser
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'GET':
+        return render(request, 'users/register.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = TestUser.objects.create(username=username, password=password)
+        return redirect('users:register')
