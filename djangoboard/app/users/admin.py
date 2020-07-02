ce4895b55c2120362c1f0df6923010e08d0c3710
hbyyy <qjaduddl94@gmail.com>
2020-07-02 17:56:26 +0900

    회원가입 template 생성

--- djangoboard/app/users/admin.py
+++ djangoboard/app/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import TestUser
+
+
+@admin.register(TestUser)
+class TestUserAdmin(admin.ModelAdmin):
+    pass
+
--- djangoboard/app/users/models.py
+++ djangoboard/app/users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class TestUser(models.Model):
+    username = models.CharField(max_length=64,
+                                verbose_name='사용자명')
+    password = models.CharField(max_length=64,
+                                verbose_name='비밀번호')
+    created_date = models.DateTimeField(auto_now_add=True,
+                                        verbose_name='등록일자')
+
+    class Meta:
+        db_table = 'testusers'
+
+
+    def __str__(self):
+        return self.username
