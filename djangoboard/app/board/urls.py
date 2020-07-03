c2218b5dd17d0e4c12bcb8ca4d8701209a9cf768
hbyyy <qjaduddl94@gmail.com>
2020-07-03 16:22:13 +0900

    board detail view 생성

--- djangoboard/app/board/urls.py
+++ djangoboard/app/board/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import board_list, board_create
+from .views import board_list, board_create, board_detail
 
 app_name = 'board'
 urlpatterns = [
     path('', board_list, name='board-list'),
     path('create/', board_create, name='board-create'),
+    path('<int:pk>/', board_detail, name='board-detail')
 ]
--- djangoboard/app/board/views.py
+++ djangoboard/app/board/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from board.decorators import login_required
-from board.forms import BoardForm
-from board.models import Board
+from users.decorators import login_required
 from users.models import TestUser
+from .forms import BoardForm
+from .models import Board
 
 
 def board_list(request):
     boards = Board.objects.order_by('-id')
     return render(request, 'board/board_list.html', {'boards': boards})
+
+
+def board_detail(request, pk):
+    board = Board.objects.get(pk=pk)
+    return render(request, 'board/board_detail.html', {'board': board})
 
 
 @login_required
